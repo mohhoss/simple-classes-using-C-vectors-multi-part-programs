b505fe2cac2e2f323cc6baa7e385527cb687f770
mohhoss <mohhoss@yahoo.com>
2017-06-04 17:01:47 -0500

    Create airsort.h

---
+++ airsort.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include "storm.h"
+using namespace std;
+#ifndef AIRSORT_H
+#define AIRSORT_H
+
+vector<Storm>  airsort( vector<Storm> &);
+
+#endif
