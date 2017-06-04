2088c9bedea54069a310eae1a06f6aa486f3037f
mohhoss <mohhoss@yahoo.com>
2017-06-04 17:04:18 -0500

    Create iterprint.h

---
+++ iterprint.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include "storm.h"
+#include "printhead.h"
+using namespace std;
+
+#ifndef ITERPRINT_H
+#define ITERPRINT_H
+
+void iterprint( vector<Storm> &);
+
+#endif
