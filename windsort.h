097bd70019a0fd54bd8fb93e9094129a16cf5e3a
mohhoss <mohhoss@yahoo.com>
2017-06-04 17:39:26 -0500

    Create windsort.h

---
+++ windsort.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include "storm.h"
+using namespace std;
+#ifndef WINDSORT_H
+#define WINDSORT_H
+
+vector<Storm>  windsort(vector<Storm> &);
+
+#endif
